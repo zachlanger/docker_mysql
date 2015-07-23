7b30114e4a1f543b1f6d4a790c3f57c1befddad8
Zach Langer <zlanger@comverge.com>
2015-07-23 16:16:09 -0600

    changes to git ignore

--- ubuntu_slave/build_ubuntu_slave.sh
+++ ubuntu_slave/build_ubuntu_slave.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 docker build -t zlanger/dockerubuntu .
-docker run -p 5000:22 --name ubuntu_slave -v $(pwd):/usr/src/app --privileged -t zlanger/dockerubuntu
+docker run -p 5000:22 --name ubuntu_slave --privileged -t zlanger/dockerubuntu
+
+# -v $(pwd):/usr/src/app   volume
