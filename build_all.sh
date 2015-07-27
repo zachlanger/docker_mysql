14f81c04d816e1f89b12246cbdccd5f164e419f4
Zach Langer <zlanger@comverge.com>
2015-07-27 16:07:54 -0600

    bash testing

--- build_all.sh
+++ build_all.sh
@@ -3,10 +3,10 @@
 
 cd jenkins_files
 docker build -t zlanger/dockerjenkins .
-docker run -p 8080:8080 --name jenkins --privileged -d zlanger/dockerjenkins
+docker run -p 8080:8080 --name jenkins -d zlanger/dockerjenkins
 cd ..
 docker cp jenkins:/root/.ssh/id_rsa.pub $PWD/ubuntu_slave
 cd ubuntu_slave
 docker build -t zlanger/dockerubuntu .
 rm id_rsa.pub
-docker run -p 5000:22 --name ubuntu_slave --privileged -t zlanger/dockerubuntu
+docker run -p 5000:22 --name ubuntu_slave -t zlanger/dockerubuntu
