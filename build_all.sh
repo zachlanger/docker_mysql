085c97b5e185fc20c0ffbe36e501af8a24b18925
Zach Langer <zlanger@comverge.com>
2015-08-03 14:26:54 -0600

    Single instruction

--- build_all.sh
+++ build_all.sh
@@ -9,6 +9,11 @@ docker cp jenkins:/root/.ssh/id_rsa.pub $PWD/ubuntu_slave
 cd ubuntu_slave
 docker build -t zlanger/dockerubuntu .
 rm id_rsa.pub
-docker run -p 5000:22 --name ubuntu_slave -t zlanger/dockerubuntu
-
-# -v $PWD/jenkins_volume:/opt/jenkins/data
+docker run -p 5000:22 --name ubuntu_slave -t zlanger/dockerubuntu &
+TASK_PID=$!
+sleep 5
+kill $TASK_PID
+docker run -p 5011:22 --name ubuntu_slave2 -t zlanger/dockerubuntu &
+TASK_PID=$!
+sleep 5
+kill $TASK_PID
